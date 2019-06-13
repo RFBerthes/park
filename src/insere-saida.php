3c99d10674afb93c0b17bfee53fc8f20d53a20ed
RFBerthes <rfbds102030@gmail.com>
2019-06-13 06:43:36 -0300

    registro ok

--- src/insere-saida.php
+++ src/insere-saida.php
@@ -7,7 +7,7 @@ $placa = $_POST['placa'];
 //echo $placa; exit;
 
 $pdo = connect_to_database("park");
-
+cd p
 $sql_ins = "INSERT INTO registros (entrada, saida, valor, veiculos_placa) VALUES (CURRENT_TIMESTAMP, NULL, NULL, :placa)";
 $stmt_ins = $pdo->prepare($sql_ins);
 $stmt_ins->bindParam(':placa', $placa);
